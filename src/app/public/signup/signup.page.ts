6b4e8fdba85ee06b53783c3b1258b4e41f26fa63
Bounteous <bounteous17@protonmail.com>
2019-05-13 19:15:11 +0200

    Confirm password signup

--- src/app/public/signup/signup.page.ts
+++ src/app/public/signup/signup.page.ts
@@ -1,20 +1,21 @@
-import { Component, OnInit, NgModule } from '@angular/core';
-import { NavController } from '@ionic/angular';
-import { UserService } from 'src/app/providers/public/user.service';
-import { UserFront } from 'src/app/models/user-front.model';
-import { CommonModule } from '@angular/common';
-import { SharedModule } from '../../shared/shared.module';
+import { Component, OnInit, NgModule } from "@angular/core";
+import { NavController } from "@ionic/angular";
+import { UserService } from "src/app/providers/public/user.service";
+import { UserFront } from "src/app/models/user-front.model";
+import { CommonModule } from "@angular/common";
+import { SharedModule } from "../../shared/shared.module";
 
 @Component({
-  selector: 'app-signup',
-  templateUrl: './signup.page.html',
-  styleUrls: ['./signup.page.scss'],
+  selector: "app-signup",
+  templateUrl: "./signup.page.html",
+  styleUrls: ["./signup.page.scss"]
 })
 @NgModule({
   imports: [CommonModule, SharedModule]
 })
 export class SignupPage implements OnInit {
   public dealer: UserFront;
+  public cPassword: String;
 
   constructor(
     private __dealerService: UserService,
@@ -22,30 +23,30 @@ export class SignupPage implements OnInit {
     public __sharedModule: SharedModule
   ) {
     this.dealer = new UserFront();
+    this.cPassword = '';
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   signUp() {
     this.__dealerService.signup(this.dealer).subscribe(
       async res => {
         await this.__sharedModule.simpleOk({
           message: res.msg,
-          duration: 4000,
-        })
-        this.goBack()
-      }, async error => {
+          duration: 4000
+        });
+        this.goBack();
+      },
+      async error => {
         await this.__sharedModule.simpleError({
           message: error.error.msg,
-          duration: 4000,
-        })
+          duration: 4000
+        });
       }
-    )
+    );
   }
 
   goBack() {
-    this.navCtrl.goBack()
+    this.navCtrl.goBack();
   }
-
 }
