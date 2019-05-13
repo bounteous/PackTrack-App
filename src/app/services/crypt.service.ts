b73698e55fed754ee96c64923dfae0e5ca9bb404
Bounteous <bounteous17@protonmail.com>
2019-05-13 19:14:16 +0200

    Custom crypt module

---
+++ src/app/services/crypt.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from "@angular/core";
+import { AES256 } from "@ionic-native/aes-256/ngx";
+
+@Injectable({
+  providedIn: "root"
+})
+export class CryptService {
+  private secureKey: string;
+  private secureIV: string;
+
+  constructor(private aes256: AES256) {}
+
+  async generateSecureKey(_random: string) {
+    return await this.aes256.generateSecureKey(_random); // Returns a 32 bytes string
+  }
+
+  async generateSecureIV(_salt: string) {
+    return await this.aes256.generateSecureIV(_salt); // Returns a 16 bytes string
+  }
+
+  async encryptMessage(_msg: string) {}
+
+  // this.aes256.encrypt(this.secureKey, this.secureIV, 'testdata')
+  //   .then(res => console.log('Encrypted Data: ',res))
+  //   .catch((error: any) => console.error(error));
+
+  // this.aes256.decrypt(this.secureKey, this.secureIV, 'encryptedData')
+  //   .then(res => console.log('Decrypted Data : ',res))
+  //   .catch((error: any) => console.error(error));
+
+  // * this.aes256.generateSecureKey('random password 12345')
+  //   .then(res => console.log('Secure Key : ',res))
+  //   .catch((error: any) => console.error(error));
+
+  // * this.aes256.generateSecureIV('random password 12345')
+  //   .then(res => console.log('Secure IV : ',res))
+  //   .catch((error: any) => console.error(error));
+}
