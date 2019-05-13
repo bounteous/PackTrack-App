9498785e40056ff7bd16205c9568ce5f45099ef0
Bounteous <bounteous17@protonmail.com>
2019-05-13 19:15:27 +0200

    Logout menu

--- src/app/pages/menu/menu.page.ts
+++ src/app/pages/menu/menu.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { Router, RouterEvent } from "@angular/router";
+import { AuthenticationService } from "../../services/authentication.service";
+import { longStackSupport } from "q";
 
 @Component({
   selector: "app-menu",
@@ -11,23 +13,38 @@ export class MenuPage implements OnInit {
   pages = [
     {
       title: "Dashboard",
+      icon: "nuclear",
       url: "/menu/(menucontent:dashboard)"
     },
     {
       title: "Chats",
+      icon: "chatboxes",
       url: "/menu/(menucontent:list-chats)"
     },
     {
       title: "New chat",
+      icon: "person-add",
       url: "/menu/(menucontent:new-chat)"
+    },
+    {
+      title: "Logout",
+      icon: "exit",
+      action: "logout"
     }
   ];
 
-  constructor(private router: Router) {
+  constructor(
+    private router: Router,
+    private __authenticationService: AuthenticationService
+  ) {
     this.router.events.subscribe((event: RouterEvent) => {
       this.selectedPath = event.url;
     });
   }
 
   ngOnInit() {}
+
+  async logout() {
+    await this.__authenticationService.logout();
+  }
 }
