573b33d4f8a4a7d0eb5d623177cb613c6dbc1c0e
Bounteous <bounteous17@protonmail.com>
2019-05-13 19:15:37 +0200

    New chat generate key

--- src/app/members/new-chat/new-chat.page.ts
+++ src/app/members/new-chat/new-chat.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { ChatService } from "src/app/providers/private/chat.service";
 import { SharedModule } from "../../shared/shared.module";
+import { CryptService } from "../../services/crypt.service";
+import { AlertController } from "@ionic/angular";
 
 @Component({
   selector: "app-new-chat",
@@ -9,10 +11,17 @@ import { SharedModule } from "../../shared/shared.module";
 })
 export class NewChatPage implements OnInit {
   public username: string;
+  public _randomKey: string;
+  public _randomSalt: string;
+
+  private secureKey: string;
+  private secureIV: string;
 
   constructor(
     private __chatService: ChatService,
-    private __sharedModule: SharedModule
+    private __sharedModule: SharedModule,
+    private __cryptService: CryptService,
+    public alertController: AlertController
   ) {}
 
   ngOnInit() {}
@@ -20,10 +29,11 @@ export class NewChatPage implements OnInit {
   create() {
     this.__chatService.create(this.username).subscribe(
       async res => {
+        await this.secretsAlertPrompt();
         await this.__sharedModule.simpleOk({
           message: res.msg,
-          duration: 3000
-        })
+          duration: 2000
+        });
       },
       async error => {
         await this.__sharedModule.simpleError({
@@ -32,5 +42,66 @@ export class NewChatPage implements OnInit {
         });
       }
     );
+  }
+
+  async genKeys() {
+    return `${await this.__cryptService.generateSecureIV(
+      this.secureIV
+    )}-${await this.__cryptService.generateSecureKey(this.secureKey)}`;
+  }
+
+  async showSecretsAlert(_key) {
+    const alert = await this.alertController.create({
+      header: "Keys generated successfully",
+      message: _key,
+      buttons: [
+        {
+          text: "Copy clipboard",
+          handler: async () => {
+            await this.__sharedModule.clipboardCopy(_key);
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  async secretsAlertPrompt() {
+    const alert = await this.alertController.create({
+      header: "Generate chat key",
+      inputs: [
+        {
+          name: "secureKey",
+          type: "text",
+          placeholder: "Key file secret"
+        },
+        {
+          name: "secureIV",
+          type: "text",
+          placeholder: "Salt for new key"
+        }
+      ],
+      buttons: [
+        {
+          text: "Cancel",
+          role: "cancel",
+          cssClass: "secondary",
+          handler: () => {
+            console.log("Confirm Cancel");
+          }
+        },
+        {
+          text: "Ok",
+          handler: async data => {
+            this.secureKey = data.secureKey;
+            this.secureIV = data.secureIV;
+            await this.showSecretsAlert(await this.genKeys());
+          }
+        }
+      ]
+    });
+
+    return await alert.present();
   }
 }
